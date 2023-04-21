3a7d04571bad204088406a6ee57f76a84dc82c7c
anjaeyoung <dkswodud011@gmail.com>
2023-04-21 16:29:20 +0900

    Fix tests shell script

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,5 +1,11 @@
-PROJECT_NAME=`GithubActions`
-PLIST_PATH=`${PROJECT_NAME}/SupportingFile`
+MINIMUM_VERSION="1.0"
+PROJECT_NAME="GithubActions"
+PLIST_PATH="${PROJECT_NAME}/SupportingFile"
 PLIST_VERSION=`defaults read  "\`pwd\`/${PLIST_PATH}/Info.plist" CFBundleShortVersionString`
 
-echo ".plist version ${PLIST_VERSION}"
+if [[ "$PLIST_VERSION" < "$MINIMUM_VERSION" ]]; then
+  echo ".plist version(${PLIST_VERSION}) must be greater than or equal to ${MINIMUM_VERSION}"
+  exit 1
+else
+  echo "Available version ${PLIST_VERSION}"
+fi
